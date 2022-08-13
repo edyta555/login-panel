9f6c878ceca7feb9c35550b653d114c472dea95d
edyta555 <edyta.badowska555@gmail.com>
2022-08-13 19:10:57 +0200

    feat: add header with log out button.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+
+import styles from "./app.module.scss";
+
 function App() {
-  return <div className="App"><p>bla bla</p></div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const logInHandler = (email, password) => {
+    setIsLoggedIn(true);
+  };
+
+  const logOutHandler = (email, password) => {
+    setIsLoggedIn(false);
+  };
+  return (
+    <div className={styles.App}>
+      <Header isLoggedIn={isLoggedIn} onLogOut={logOutHandler} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import styles from "./header.module.scss";
+
+const Header = ({ isLoggedIn, onLogOut }) => {
+  return (
+    <div className={styles.headerContainer}>
+      <div>
+        <h3>A sample page</h3>
+      </div>
+      <nav className={styles.navigation}>
+        {isLoggedIn && (
+          <ul>
+            <li>
+              <a href="/">Profile</a>
+            </li>
+            <li>
+            <button onClick={onLogOut}>Log Out</button>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
